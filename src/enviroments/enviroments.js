2857f461dd9f1cc52597356af1c904d9037ba407
dani_alva <dani0alva0nino@gmail.com>
2022-09-02 14:39:17 -0500

    subir ruta heroku

--- src/enviroments/enviroments.js
+++ src/enviroments/enviroments.js
@@ -1,2 +1,2 @@
 /*export const URL_BACKEND = 'http://127.0.0.1:8000'*/
-export const URL_BACKEND = 'https://sistwebturismomain.vercel.app'
+export const URL_BACKEND = 'https://sistwebturismocog15.herokuapp.com'
